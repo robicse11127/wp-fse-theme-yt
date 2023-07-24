8dcd734618d8fa2065feca55ee02445f61d601d1
robicse11127 <robizmailbox@gmail.com>
2023-07-24 18:39:42 +0600

    FSE theme for the YT

---
+++ patterns/hidden-404.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: RS Hidden 404
+ * Slug: fse-wp-2023/hidden-404
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:image {"align":"center","id":299,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image aligncenter size-large"><img src="<?php echo get_template_directory_uri() . '/assets/img/404.jpg' ?>" alt="404 image" class="wp-image-299"/></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"82px"}},"textColor":"vivid-red"} -->
+    <h1 class="wp-block-heading has-text-align-center has-vivid-red-color has-text-color" style="font-size:82px">404 Not Found!</h1>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"center","level":4,"fontSize":"medium"} -->
+    <h4 class="wp-block-heading has-text-align-center has-medium-font-size">Are you lost? Please search for something more meaningful!</h4>
+    <!-- /wp:heading -->
+</div>
+<!-- /wp:group -->
